bafdb4a1f344f8c0c25cdc363b340b0878207036
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-11-13 18:32:09 +0530

    fhghghghf

--- models/mehndiCategory-model.js
+++ models/mehndiCategory-model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const mehndiCategorySchema = new mongoose.Schema({
     categoryName: { type: String },
     description: { type: String },
-   
+    image: { type: String },
     status: { type: String, default: "ACTIVE" },
 }, { timestamps: true });
 
